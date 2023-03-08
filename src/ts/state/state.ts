80c36971afeedf15e2dca424baebf3c8dca96a35
Aloysius Kurnia Mahendra <aloysius1234@yahoo.co.id>
2023-03-08 23:01:15 +0700

    Add states

--- src/ts/state/state.ts
+++ src/ts/state/state.ts
@@ -1,16 +1,85 @@
+import { EventManager } from "observer/event-manager";
+
+type MaybeAppState = AppState | undefined;
+
 export abstract class AppState {
     protected readonly eventManager: EventManager;
-    onControlPointClick(point: ControlPoint): void { }
-    onSegmentClick(segment: CurveSegment): void { }
-    onMouseMove(x: number, y: number): void { }
-    onEscape(): void { }
-    onSpace(): void { }
-    onLetterB(): void { }
-    onLetterR(): void { }
-    onReleaseR(): void { }
-    onLetterF(): void { }
-    onNumber1(): void { }
-    onNumber2(): void { }
-    onNumber3(): void { }
-    onNumber4(): void { }
+    onControlPointClick(point: ControlPoint): MaybeAppState { return undefined; }
+    onSegmentClick(segment: CurveSegment): MaybeAppState { return undefined; }
+    onMouseMove(x: number, y: number): MaybeAppState { return undefined; }
+    onEscape(): MaybeAppState { return undefined; }
+    onSpace(): MaybeAppState { return undefined; }
+    onLetterB(): MaybeAppState { return undefined; }
+    onLetterR(): MaybeAppState { return undefined; }
+    onReleaseR(): MaybeAppState { return undefined; }
+    onLetterF(): MaybeAppState { return undefined; }
+    onNumber1(): MaybeAppState { return undefined; }
+    onNumber2(): MaybeAppState { return undefined; }
+    onNumber3(): MaybeAppState { return undefined; }
+    onNumber4(): MaybeAppState { return undefined; }
+}
+
+export class StateTracker {
+    private currentState: AppState;
+    constructor(eventManager: EventManager) {
+        this.currentState = new IdleState(eventManager);
+    }
+
+    private changeStateIfNecessary(newState: MaybeAppState) {
+        if (newState) {
+            this.currentState = newState;
+        }
+    }
+
+    onControlPointClick(point: ControlPoint) {
+        this.changeStateIfNecessary(this.currentState.onControlPointClick(point));
+    }
+
+    onSegmentClick(segment: CurveSegment) {
+        this.changeStateIfNecessary(this.currentState.onSegmentClick(segment));
+    }
+
+    onMouseMove(x: number, y: number) {
+        this.changeStateIfNecessary(this.currentState.onMouseMove(x, y));
+    }
+
+    onEscape() {
+        this.changeStateIfNecessary(this.currentState.onEscape());
+    }
+
+    onSpace() {
+        this.changeStateIfNecessary(this.currentState.onSpace());
+    }
+
+    onLetterB() {
+        this.changeStateIfNecessary(this.currentState.onLetterB());
+    }
+
+    onLetterR() {
+        this.changeStateIfNecessary(this.currentState.onLetterR());
+    }
+
+    onReleaseR() {
+        this.changeStateIfNecessary(this.currentState.onReleaseR());
+    }
+
+    onLetterF() {
+        this.changeStateIfNecessary(this.currentState.onLetterF());
+    }
+
+    onNumber1() {
+        this.changeStateIfNecessary(this.currentState.onNumber1());
+    }
+
+    onNumber2() {
+        this.changeStateIfNecessary(this.currentState.onNumber2());
+    }
+
+    onNumber3() {
+        this.changeStateIfNecessary(this.currentState.onNumber3());
+    }
+
+    onNumber4() {
+        this.changeStateIfNecessary(this.currentState.onNumber4());
+    }
 }

0a8d2be740970b30aad0f861153413a7a58fb64f
Aloysius Kurnia Mahendra <aloysius1234@yahoo.co.id>
2023-03-13 08:29:40 +0700

    implement canvas

--- src/ts/canvas.ts
+++ src/ts/canvas.ts
@@ -1,1 +1,26 @@
-export class Canvas extends ElementWra
+import { BezierWrapper, GroupWrapper, SVGWrapper } from "util/svg-wrapper";
+
+export class Canvas extends SVGWrapper {
+    private segmentGroup: GroupWrapper;
+    private controlPointGroup: GroupWrapper;
+    constructor(parent: HTMLElement) {
+        super();
+        this.setAttribute("width", "100%");
+        this.setAttribute("height", "100%");
+        
+        this.segmentGroup = new GroupWrapper();
+        this.controlPointGroup = new GroupWrapper();
+        this.segmentGroup.appendTo(this);
+        this.controlPointGroup.appendTo(this);
+
+        this.appendToElement(parent);
+    }
+
+    addSplineSegment(segment: BezierWrapper) {
+        segment.appendTo(this.segmentGroup);
+    }
+
+    addControlPoint(point: BezierWrapper) {
+        point.appendTo(this.controlPointGroup);
+    }
+}

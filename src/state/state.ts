4e2e1563a56390ebc576bc92d59c66b5dc67bda7
Aloysius Kurnia Mahendra <aloysius1234@yahoo.co.id>
2023-03-13 10:54:11 +0700

    Clean up app state

--- src/state/state.ts
+++ src/state/state.ts
@@ -1,28 +1,25 @@
 import { ControlPoint } from "element/elements/control-point";
 import { SplineSegment } from "element/elements/spline-segment";
-import { ControlManager } from "observer/control-manager";
-
-type MaybeAppState = AppState | undefined;
 
 export abstract class AppState {
-    constructor(private stateTracker: StateTracker) {}
+    constructor(private stateTracker: StateTracker) { }
     protected changeState(state: AppState) {
         this.stateTracker.setCurrentState(state);
     }
 
-    onControlPointClick(point: ControlPoint): MaybeAppState { return undefined; }
-    onSegmentClick(segment: SplineSegment): MaybeAppState { return undefined; }
-    onMouseMove(x: number, y: number): MaybeAppState { return undefined; }
-    onEscape(): MaybeAppState { return undefined; }
-    onSpace(): MaybeAppState { return undefined; }
-    onLetterB(): MaybeAppState { return undefined; }
-    onLetterR(): MaybeAppState { return undefined; }
-    onReleaseR(): MaybeAppState { return undefined; }
-    onLetterF(): MaybeAppState { return undefined; }
-    onNumber1(): MaybeAppState { return undefined; }
-    onNumber2(): MaybeAppState { return undefined; }
-    onNumber3(): MaybeAppState { return undefined; }
-    onNumber4(): MaybeAppState { return undefined; }
+    onControlPointClick(point: ControlPoint) { }
+    onSegmentClick(segment: SplineSegment) { }
+    onMouseMove(x: number, y: number) { }
+    onEscape() { }
+    onSpace() { }
+    onLetterB() { }
+    onLetterR() { }
+    onReleaseR() { }
+    onLetterF() { }
+    onNumber1() { }
+    onNumber2() { }
+    onNumber3() { }
+    onNumber4() { }
 }
 
 export interface StateTracker {
